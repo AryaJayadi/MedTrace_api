fd766a3f108f43f23b4a7b1676a1bce92ddda649
Arya <arya.jayadi2013@gmail.com>
2025-05-03 09:13:50 +0700

    feat: drug

---
+++ internal/models/drug.go
@@ -0,0 +1,11 @@
+package models
+
+type Drug struct {
+	ID          string `json:"ID"`          // Unique drug ID
+	Name        string `json:"Name"`        // Drug name
+	Description string `json:"Description"` // Description
+	BatchID     string `json:"BatchID"`     // Reference to Batch.ID
+	Owner       string `json:"Owner"`       // Current owner
+	Location    string `json:"Location"`    // Current location
+	Status      string `json:"Status"`      // e.g., Manufactured, InTransit, Delivered, Expired
+}
