101de0e909e2ad494a19ff89f5764df979b2b543
Arya <arya.jayadi2013@gmail.com>
2025-05-03 09:12:53 +0700

    feat: batch

---
+++ internal/models/batch.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+type Batch struct {
+	ID             string    `json:"ID"`             // Unique batch ID
+	Manufacturer   string    `json:"Manufacturer"`   // Manufacturer name
+	ManufacturedAt time.Time `json:"ManufacturedAt"` // Manufacture timestamp
+	ExpiryDate     time.Time `json:"ExpiryDate"`     // Expiry date for all drugs in the batch
+	Drugs          []string  `json:"Drugs"`          // List of Drug IDs in this batch
+}
