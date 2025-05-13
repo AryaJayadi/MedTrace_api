fca60744f22dfc2b0d218ca804d0874f7480b057
Arya <arya.jayadi2013@gmail.com>
2025-05-13 16:02:38 +0700

    feat: update models

--- internal/models/dto/batch/create_batch.go
+++ internal/models/dto/batch/create_batch.go
@@ -5,11 +5,12 @@ import (
 )
 
 type BatchCreate struct {
-	Amount       int       `json:"Amount" xml:"Amount" form:"Amount"`                   // Amount of drugs in the batch
-	Description  string    `json:"Description" xml:"Description" form:"Description"`    // Drug description
-	Manufacturer string    `json:"Manufacturer" xml:"Manufacturer" form:"Manufacturer"` // Manufacturer name
-	Name         string    `json:"Name" xml:"Name" form:"Name"`                         // Drug name
-	ExpiryDate   time.Time `json:"ExpiryDate" xml:"ExpiryDate" form:"ExpiryDate"`       // Drug expiry date
-	Location     string    `json:"Location" xml:"Location" form:"Location"`             // Location of the drug
-	Status       string    `json:"Status" xml:"Status" form:"Status"`                   // Drug status
+	Amount         int       `json:"Amount" xml:"Amount" form:"Amount"`                         // Amount of drugs in the batch
+	Description    string    `json:"Description" xml:"Description" form:"Description"`          // Drug description
+	Manufacturer   string    `json:"Manufacturer" xml:"Manufacturer" form:"Manufacturer"`       // Manufacturer name
+	Name           string    `json:"Name" xml:"Name" form:"Name"`                               // Drug name
+	ExpiryDate     time.Time `json:"ExpiryDate" xml:"ExpiryDate" form:"ExpiryDate"`             // Drug expiry date
+	Location       string    `json:"Location" xml:"Location" form:"Location"`                   // Location of the drug
+	ProductionDate time.Time `json:"ProductionDate" xml:"ProductionDate" form:"ProductionDate"` // Drug production date
+	Status         string    `json:"Status" xml:"Status" form:"Status"`                         // Drug status
 }
