12f7c85d741b00421867542807035da7e15b12d0
= <Arya@val.id>
2025-05-31 14:07:46 +0700

    feat: fixed json object for LoginResponseData

--- internal/models/entity/login.go
+++ internal/models/entity/login.go
@@ -1,7 +1,7 @@
 package entity
 
 type LoginResponseData struct {
-	Token   string `json:"token"`
-	OrgID   string `json:"orgId"`
-	Message string `json:"message"`
+	Token   string `json:"Token"`
+	OrgID   string `json:"OrgId"`
+	Message string `json:"Message"`
 }
