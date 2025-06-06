8a79d4770a5f5545e29050f3a0047ed4a0afcaec
= <Arya@val.id>
2025-06-06 15:39:27 +0700

    feat: add location to drug

--- internal/models/entity/drug.go
+++ internal/models/entity/drug.go
@@ -5,6 +5,7 @@ type Drug struct {
 	ID            string `json:"ID"`
 	BatchID       string `json:"BatchID"`
 	OwnerID       string `json:"OwnerID"`
+	Location      string `json:"Location"`
 	IsTransferred bool   `json:"isTransferred"`
 	TransferID    string `json:"TransferID,omitempty"` // omitempty if it might not always be present
 }
